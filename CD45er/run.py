03617dd83e418d2b3f279a6ed2d1b483c98e45fb
njharlen <njharlen@broadinstitute.org>
2022-03-24 15:14:04 +0000

    Commented and cleaned up code

--- CD45er/run.py
+++ CD45er/run.py
@@ -12,6 +12,7 @@ import pickle
 isoforms = np.array(['RX','RAX','RBX','RABX','RBCX','RABCX'])
 segments = np.array(['R','A','B','C','X'])
 
+# Iterates over a 10x bam file and compiles isoform compatibility and position for every read
 def annotate_reads(bam,E):
 
     # Maps genomic position to exon number
@@ -19,18 +20,16 @@ def annotate_reads(bam,E):
     pos_max = E['en'].max()
     chrom = E['chr'].unique()[0]
     exons = np.zeros(pos_max-pos_min).astype(int)
-
     for ind,g in E.iterrows():
         exons[(g['st']-pos_min):(g['en']-pos_min)]=ind+1
 
-    segments = np.array(['R','A','B','C','X'])
-
+    # Begin read annotation
     samfile = pysam.AlignmentFile(bam, "rb")
-
     res = list()
 
     for read in samfile.fetch(chrom,pos_min,pos_max):
         tags = dict(read.get_tags())
+        # Ignore reads without an assigned cell barcode or UMI
         if (not 'CB' in tags.keys()) or (not 'UB' in tags.keys()):
             continue
         else:
@@ -39,30 +38,31 @@ def annotate_reads(bam,E):
     
         exon_seen = np.zeros(33)
 
-        # Last position in an exon supported by read
+        # Record last position in an exon supported by a read
         last_pos = -1
 
-
         for pos in read.get_reference_positions():
-        
+            # If not currently in an exon, continue
             if (pos<(pos_min-1)) or (pos>=(pos_max-1)):
                 continue
-        
-            exon=exons[pos-(pos_min-1)]
-        
+
+            # Look up which exon we're in and record
+            exon=exons[pos-(pos_min-1)]        
             if exon>0:
                 exon_seen[exon-1]=1
                 last_pos=pos
+
+        # If no exons covered, ignore
         if sum(exon_seen)==0:
             continue
-            
+        
+        # Create a string of blocks R(exon3)-A-B-C-X(exon7) of exons supported by read
         segments_seen = "".join(segments[exon_seen[2:7]==1])
     
 
-        # Fragment length if all exons are included
+        # Record in which exon the read ended, and at what position
         last_exon = np.where(exon_seen)[0][-1]
         exon_pos=last_pos-E.loc[last_exon,'st']
-    
     
         res.append(pd.Series({'readname':read.qname,'CB':cb,'UB':ub,'seg':segments_seen,'end':last_pos,
                           'last_exon':last_exon,'exon_pos':exon_pos}))
@@ -73,6 +73,10 @@ def annotate_reads(bam,E):
     
     return(df)
 
+# Giving a dataframe of annotated reads, performs EM to estimate 
+#  - the isoform probability of every transcript (UMI)
+#  - the overall frequency of different isoforms (pi)
+#  - The fragment length distribution parameters mu/sig2 (only if not provided)
 def isoform_quantification(df,E,mu=None,sig2=None,max_fraglen=500):
 
     exon_map = {'A':4,'B':5,'C':6}
@@ -85,6 +89,7 @@ def isoform_quantification(df,E,mu=None,sig2=None,max_fraglen=500):
         iso = isoforms[i].replace("R","").replace("X","")
         for exon in iso:
             exon_inc[exon_map[exon]-1,i] = 1
+
     # Pre-compute exon x isoform matrix of transcript lengths to each exon
     shifts = np.zeros((E.shape[0],len(isoforms)))
 
@@ -93,14 +98,14 @@ def isoform_quantification(df,E,mu=None,sig2=None,max_fraglen=500):
         elen[exon_inc[:,i]==0]=0
         shifts[:,i] = [0] + list(elen.cumsum()[0:-1])
     
-    max_fraglen = 500
     exon_last_use = np.where(shifts.min(axis=1)>=max_fraglen)[0][0]
 
     idx = (df['last_exon']>=exon_last_use)
 
-    # Fraction reads far 3'
+    # Fraction reads too far 3' 
     print(f"Filtering {sum(idx)}/{len(idx)} reads with minimum possible length >={max_fraglen}")
 
+    # Reads ending before the A segment will have equal likelihood to all isoforms
     splicing_informative=~idx&(df['last_exon']>=4)
     print(f"{sum(splicing_informative)}/{len(splicing_informative)} reads informative for splicing")
 
@@ -120,8 +125,7 @@ def isoform_quantification(df,E,mu=None,sig2=None,max_fraglen=500):
     # Determine compatibility matrix
     compat = np.array([[seg in iso for iso in isoforms] for seg in df['seg']])
 
-    # Make estiates of fragment length params
-    
+    # Seed initial values for mu/sig2 (to be later optimized)
     if (mu is None) and (sig2 is None):
         mu = np.median(frag_len.min(axis=1))
         sig2 = frag_len[frag_len<=600].std()**2
@@ -129,8 +133,11 @@ def isoform_quantification(df,E,mu=None,sig2=None,max_fraglen=500):
         estimate_fraglen_dist = True
     else:
         estimate_fraglen_dist = False
+
+    # Initialize isoform class weights uniformly
     pi = np.ones(len(isoforms))/len(isoforms)
 
+    # Run EM
     mu,sig2,pi,dfup = EM(df,frag_len,compat,mu,sig2,pi,estimate_fraglen_dist=estimate_fraglen_dist)
 
     print('Estimated parameters:')
@@ -143,6 +150,7 @@ def isoform_quantification(df,E,mu=None,sig2=None,max_fraglen=500):
 
     return(mu,sig2,pis,dfup)
 
+# Run EM engine
 def EM(df,frag_len,compat,mu,sig2,pi,nit=100,estimate_fraglen_dist=True):
 
     for i in range(0,nit):
@@ -181,6 +189,10 @@ def EM(df,frag_len,compat,mu,sig2,pi,nit=100,estimate_fraglen_dist=True):
 
     return(mu,sig2,pi,dfup)
 
+## Takes two inputs:
+### (1) A 10x bam with CB/UB tags consistent with cellranger processing
+### (2) A gtf-format file with the exon locations of CD45
+
 def main():
     bam_path = sys.argv[1]
     exon_path = sys.argv[2] 
@@ -190,13 +202,15 @@ def main():
     E = E.sort_values(['st','en']).reset_index(drop=True)
     E['len'] = E['en']-E['st']+1
     
+    # Annotate reads with consistency to each isoform position
     df = annotate_reads(bam_path,E)
-
     df.to_csv('CD45_annotated_reads.csv',index=False)
     
+    # Run EM algorithm to estimate isoforms
     mu,sig2,pi,p_iso = isoform_quantification(df,E,max_fraglen=500)
     p_iso.to_csv('isoform_probabilities.csv',index=False)
 
+    # Save output
     with open("parameters.pkl","wb") as f:
         pickle.dump({'mu':mu,'sig2':sig2,'pi':pi,'p_iso':p_iso},f)
 
